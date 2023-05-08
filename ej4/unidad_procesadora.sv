bdf8f9b4ceb14623ddd06f25336f4a15dc1a32e5
joaquin <joaquindosmil@gmail.com>
2023-05-08 15:31:33 -0300

    texto explicativo

--- ej4/unidad_procesadora.sv
+++ ej4/unidad_procesadora.sv
@@ -4,29 +4,35 @@ module unidad_procesadora #(parameter N=4)
 								input logic [N-1:0] DATA_IN,
 								output logic [3:0] stateBits,
 								output logic [N-1:0] DATA_out,Address_out);
-			logic we, B_sel, MF_sel, MD_sel;
+
+			//Variables intermedias para palabra de control
+			logic we, MB_sel, MF_sel, MD_sel;
 			logic [1:0] A,B,D,H;
 			logic [3:0] G;
 			//Distribuimos la palabra de control
-			assign A= ctrl_word[15:14];
-			assign B= ctrl_word[13:12];
-			assign D= ctrl_word[11:10];
-			assign we=ctrl_word[9];
-			assign B_sel=ctrl_word[8];
-			assign G=ctrl_word[7:4];
-			assign H=ctrl_word[3:2];
-			assign MF_sel=ctrl_word[1];
-			assign MD_sel=ctrl_word[0];
+			assign A= ctrl_word[15:14];	//registro A
+			assign B= ctrl_word[13:12]; 	//registro B
+			assign D= ctrl_word[11:10]; 	//registro destino 
+			assign we=ctrl_word[9];     	//activacion escritura registros
+			assign MB_sel=ctrl_word[8]; 	//Mux (B o constante)
+			assign G=ctrl_word[7:4];    	//Selector ALU
+			assign H=ctrl_word[3:2];    	//Selector unidad corrimiento		
+			assign MF_sel=ctrl_word[1]; 	//Mux (ALU o shifter)
+			assign MD_sel=ctrl_word[0]; 	//Mux (MF_sel o data in)
+	
 			//Variables Intermedias
 			logic [N-1:0] Ra,Rb,ALU_out,SHIFTTER_out,Y,F1,F2;
 			logic [3:0] flags;
 			logic [3:0] Constant_IN;
-			assign Constant_IN=4'b1111;
-			reg_controller REG_FILE(clk,we,A,B,D,Ra,Rb,F2);
-			alu_flags alu(Ra,B_sel ? Constant_IN : Rb,G[3:1],G[0],ALU_out,flags);
-			shiffter SHIFTER_unit(B_sel ? Constant_IN : Rb,H,SHIFTTER_out);
-			assign Y=B_sel ? Constant_IN : Rb;
-			assign DATA_out=B_sel ? Constant_IN : Rb;
+
+			//asignaciones y modulos
+			assign Constant_IN=4'b1111; //valor constante 
+			assign Y=MB_sel ? Rb : Constant_IN;
+			reg_controller REG_FILE(clk,we,A,B,D,Ra,Rb,F2); //modulo registros 
+			alu_flags alu(Ra,Y,G[3:1],G[0],ALU_out,flags); //modulo alu
+			shiffter SHIFTER_unit(Y,H,SHIFTTER_out); //modulo unidad corrimiento
+			assign Address_out = Ra;
+			assign DATA_out= Y;
 			assign F1 = MF_sel ? SHIFTTER_out : ALU_out;
 			assign F2 =  MD_sel ? DATA_IN :F1;
 			
