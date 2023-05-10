e44f90270d2cf7bd07f2968418112dcab65f88df
joaquin pappano <114824831+PappasFlow@users.noreply.github.com>
2023-05-09 22:07:58 -0300

    Constant_IN

--- Tests_unidad_procesadora/test_basic.sv
+++ Tests_unidad_procesadora/test_basic.sv
@@ -3,17 +3,17 @@ module test();
 
 	logic clk;
 	logic [15:0] control;
-	logic [3:0] datain,dataout,adr_out;
+	logic [3:0] datain,dataout,adr_out,Constant_IN;
 	logic [3:0] flags;
 	logic [19:0] testvector [N-1:0]; 
 	integer i;
 	
-	unidad_procesadora dat(clk,control,datain,flags,dataout,adr_out);
+	unidad_procesadora dat(clk,control,datain,Constant_IN,flags,dataout,adr_out);
 	
 	initial begin
 		$readmemb("D:/CargaRegistro.txt",testvector);
 		for(i=0;i<N;i=i+1) begin
-			{control,datain} = testvector[i];
+			{control,Constant_IN,datain} = testvector[i];
 			clk = 0;
 			#10;
 			clk = 1;
